6a07ce7a4aaaa246abc589eec40f828e5e86d31e
Khalel <KAbaquin@west-mec.org>
2019-10-09 15:21:20 -0700

    Update 6
    installed pug, added css, working on making the local host work properly, added a bootstrap card

--- Projects/poké_1.js
+++ Projects/poké_1.js
@@ -1,3 +1,16 @@
-// const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 8080;
+    app.use(express.static(__dirname));
+    app.listen(port, function() {
+        console.log('listening on 8080', port);
+    });
 
-fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
+// var express = require('express');
+// var app = express();
+//     var port = 8080;
+//     app.use(express.static(__dirname));
+
+//     app.listen(port, function() {
+//         console.log('Server listening on localhost:%s', port);
+//     });
