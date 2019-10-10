26040baa38a10706736536cd3df0752d4ff152a7
Khalel <KAbaquin@west-mec.org>
2019-10-10 15:23:30 -0700

    Update 7
    made some adjustments to the navbar, mostly added css

--- Projects/poké_1.js
+++ Projects/poké_1.js
@@ -1,10 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 8080;
-    app.use(express.static(__dirname));
-    app.listen(port, function() {
-        console.log('listening on 8080', port);
-    });
 
 // var express = require('express');
 // var app = express();
