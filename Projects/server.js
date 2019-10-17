9aa9d8dd6e2151faef6c47e9d99bb44939c96951
Khalel <KAbaquin@west-mec.org>
2019-10-17 15:27:03 -0700

    Update 10
    still figuring out how to use the 'pug' template engine

--- Projects/server.js
+++ Projects/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+
     app.use(express.static(__dirname));
     app.listen(port, function() {
         console.log('listening on', port);
     });
 
-    // app.set('view engine', 'pug');
+    app.set('view engine', 'pug');
 
-    // app.get('../', function (req, res) {
-    //     res.render('index', { title: 'Hey', message: 'Hello there!' })
-    //   });
+    // app.use
+
+    // app.get('/', function(req, res) {
+    //     res.sendFile(__dirname + '/index.html');
+    // });
+
+    // app.get('/magicCarpet', function(req, res) {
+    //     res.sendFile(__dirname + '/magicCarpet.html');
+    // });
+
+    // app.get('/lint', function(req, res) {
+    //     res.render('index', {pokémon: });
+    // });
+
